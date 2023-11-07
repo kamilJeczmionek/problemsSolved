d6d2c859250f303ee35e444c421679953da19b57
Kamil Jęczmionek <jeczmionek.kamil@gmail.com>
2023-11-07 21:13:04 +0100

    Update TwoSum.java

--- LeetcodeSolutions/TwoSum.java
+++ LeetcodeSolutions/TwoSum.java
@@ -4,7 +4,6 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.*/
 
-package LeetcodeSolutions;
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
