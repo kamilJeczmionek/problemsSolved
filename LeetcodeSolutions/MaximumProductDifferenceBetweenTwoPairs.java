0d4ce2e714048ad62bc6ed85783bcb8e115783b2
Kamil Jęczmionek <jeczmionek.kamil@gmail.com>
2023-11-08 20:35:02 +0100

    Update MaximumProductDifferenceBetweenTwoPairs.java

--- LeetcodeSolutions/MaximumProductDifferenceBetweenTwoPairs.java
+++ LeetcodeSolutions/MaximumProductDifferenceBetweenTwoPairs.java
@@ -1,7 +1,10 @@
-/*The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+/*
+Leetcode problem 1913. Maximum Product Difference Between Two Pairs
+The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
 For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
 Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
-Return the maximum such product difference.*/
+Return the maximum such product difference.
+*/
 class Solution {
         public int maxProductDifference(int[] nums) {
             int max1, max2, min1, min2;
