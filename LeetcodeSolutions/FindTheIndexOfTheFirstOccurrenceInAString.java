7be89561e761e3c94622da440204b12a7c558ae8
Kamil Jęczmionek <jeczmionek.kamil@gmail.com>
2023-11-14 22:07:16 +0100

    Create FindTheIndexOfTheFirstOccurrenceInAString.java

---
+++ LeetcodeSolutions/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,10 @@
+/*
+Leetcode problem 28. Find The Index Of The First Occurrence In A String
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+*/
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
