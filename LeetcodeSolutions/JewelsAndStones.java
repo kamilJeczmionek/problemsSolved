588863dcf65acf433ff079966d9b7e606f15dfa4
Kamil Jęczmionek <jeczmionek.kamil@gmail.com>
2023-11-23 14:09:49 +0100

    Create JewelsAndStones.java

---
+++ LeetcodeSolutions/JewelsAndStones.java
@@ -0,0 +1,17 @@
+/*
+Leetcode problem 771. Jewels and Stones
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. 
+You want to know how many of the stones you have are also jewels.
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+*/
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int counter = 0;
+        for(int i = 0; i < jewels.length(); i++){
+            for(int j = 0; j < stones.length(); j++){
+                if(jewels.charAt(i) == stones.charAt(j)) counter++;
+            }
+        }
+        return counter;
+    }
+}
