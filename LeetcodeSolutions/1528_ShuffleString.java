b2bf0fad81a2f39eeb1facea2f1bc7691e307331
Kamil Jęczmionek <jeczmionek.kamil@gmail.com>
2023-12-04 19:29:54 +0100

    Create 1528_ShuffleString.java

---
+++ LeetcodeSolutions/1528_ShuffleString.java
@@ -0,0 +1,16 @@
+/*
+Leetcode problem 1528. Shuffle tring
+You are given a string s and an integer array indices of the same length. 
+The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+Return the shuffled string.
+*/
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char arr[] = new char[s.length()];
+        for(int i = 0; i < s.length(); i++){
+            arr[indices[i]] = s.charAt(i);
+        }
+        String ans = new String(arr);
+        return ans;
+    }
+}
